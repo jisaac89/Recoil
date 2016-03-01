da70c8c0356305f38c181c5facfab7d933aaeddd
joseph <josephisaac89@gmail.com>
2016-03-01 10:06:25 -0700

    removing selected item from grid working on a fix for it right now

--- src/components/Grid/GridBody.tsx
+++ src/components/Grid/GridBody.tsx
@@ -4,10 +4,75 @@ import Layer from '../Layer/Layer';
 import Button from '../Button/Button';
 import Emerge from '../Emerge/Emerge';
 
-export default class GridBody extends React.Component<any, any>{
-  public onSelect(key) {
-    this.props.onSelect(key);
+
+class GridRow extends React.Component<any,any>{
+
+  constructor() {
+    super();
+    this.state = {
+      selected: false
+    }
   }
+
+  public componentWillReceiveProps(nextProps) {
+    this.setState({
+      selected: nextProps.selected
+    })
+  }
+
+  public onSelect(item) {
+    this.props.onSelect(item);
+    this.props.selected(item);
+  }
+
+  public render(){
+
+    const self = this;
+    const props = self.props;
+
+    let {columns, dataSource, i} = props;
+
+    let columnArray = [];
+    for (let x = 0; x < columns.length; x++) {
+      columnArray.push(
+        <Layer flex flow="column nowrap" justify="center" align="flex-start" style={{width: columns[x].width}} key={i+x} className="ptb5 w100">
+          {(()=>{
+            if (columns[x].tabbable) {
+              return (
+                <Button ghost>
+                  {dataSource[i][columns[x].name]}
+                </Button>
+              )
+            } else if (columns[x].template) {
+                return (
+                  <Layer>
+                    {columns[x].template(dataSource[i])}
+                  </Layer>
+                )
+              } else {
+              return (
+                <Layer>
+                  <small>{dataSource[i][columns[x].name]}</small>
+                </Layer>
+              )
+            }
+          })()}
+        </Layer>
+      )
+    }
+
+    return (
+      <div className={"posrel w100 flohide " + (this.state.selected ? 'selected' : null)}>
+        <Layer flex flow="row nowrap" justify="flex-start" className="posrel w100" onClick={this.onSelect.bind(this, self.props.dataSource[i])}>
+          {columnArray}
+        </Layer>
+        <Selectable checked={this.state.selected ? true : false} />
+      </div>
+    )
+  }
+}
+
+export default class GridBody extends React.Component<any, any>{
 
   render(){
 
@@ -19,43 +84,8 @@ export default class GridBody extends React.Component<any, any>{
     let rowArray = [];
 
     for (let key in self.props.dataSource) {
-
-      let columnArray = [];
-      for (let x = 0; x < columns.length; x++) {
-        columnArray.push(
-          <Layer flex flow="column nowrap" justify="center" align="flex-start" style={{width: columns[x].width}} key={key+x} className="ptb5 w100">
-            {(()=>{
-              if (columns[x].tabbable) {
-                return (
-                  <Button ghost>
-                    {dataSource[key][columns[x].name]}
-                  </Button>
-                )
-              } else if (columns[x].template) {
-                  return (
-                    <Layer>
-                      {columns[x].template(dataSource[key])}
-                    </Layer>
-                  )
-                } else {
-                return (
-                  <Layer>
-                    <small>{dataSource[key][columns[x].name]}</small>
-                  </Layer>
-                )
-              }
-            })()}
-          </Layer>
-        )
-      }
-
       rowArray.push(
-        <div className={"posrel w100 flohide " + (props.selected.has(dataSource[key].id) ? 'selected' : null)} key={key}>
-          <Layer flex flow="row nowrap" justify="flex-start" className="posrel w100" onClick={this.onSelect.bind(this, self.props.dataSource[key])}>
-          {columnArray}
-          </Layer>
-          <Selectable checked={props.selected.has(dataSource[key].id) ? true : false} />
-        </div>
+        <GridRow key={key} i={key} {...props} />
       )
     }
 
