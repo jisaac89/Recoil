24ce22218c1f2546b577367c9caac5905f6acbbe
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-05-24 18:27:01 -0700

    Fixed publish and export.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,21 @@
-// for typescript we need to decalre module first.
-
-declare var module : any
-
-export * from './components/Align/Align';
-export * from './components/Button/Button';
-export * from './components/Card/Card';
-export * from './components/Checkbox/Checkbox';
-export * from './components/Door/Door';
-export * from './components/Dropdown/Dropdown';
-export * from './components/Emerge/Emerge';
-export * from './components/Grid/Grid';
-export * from './components/Input/Input';
-export * from './components/Layer/Layer';
-export * from './components/Loading/Loading';
-export * from './components/Modal/Modal';
-export * from './components/Pane/Pane';
-export * from './components/Selectable/Selectable';
-export * from './components/Shrink/Shrink';
-export * from './components/Toolbar/Toolbar';
-export * from './components/Transform/Transform';
-export * from './components/Wizard/Wizard';
-export * from './components/Shortcut/Shortcut';
+export {default as Align} from './components/Align/Align';
+export {default as Button} from './components/Button/Button';
+export {default as Card} from './components/Card/Card';
+export {default as Checkbox} from './components/Checkbox/Checkbox';
+export {default as Door} from './components/Door/Door';
+export {default as Dropdown} from './components/Dropdown/Dropdown';
+export {default as Emerge} from './components/Emerge/Emerge';
+export {default as Grid} from './components/Grid/Grid';
+export {default as Input} from './components/Input/Input';
+export {default as Layer} from './components/Layer/Layer';
+export {default as Loading} from './components/Loading/Loading';
+export {default as Modal} from './components/Modal/Modal';
+export {default as Pane} from './components/Pane/Pane';
+export {default as Selectable} from './components/Selectable/Selectable';
+export {default as Shrink} from './components/Shrink/Shrink';
+export {default as Toolbar} from './components/Toolbar/Toolbar';
+export {default as Transform} from './components/Transform/Transform';
+export {default as Wizard} from './components/Wizard/Wizard';
+export {default as Shortcut} from './components/Shortcut/Shortcut';
 
 import './less/Recoil.less';
