239557716b94f7d67346ebd8e9852e0ae250afa9
joseph isaac <josephisaac89@gmail.com>
2016-10-07 08:56:22 -0700

    open updates

--- src/components/Open/Open.tsx
+++ src/components/Open/Open.tsx
@@ -3,6 +3,14 @@ import * as ReactDOM from 'react-dom';
 import * as classNames from 'classnames';
 import './Open.less';
 
+
+// state machine so it should
+// on inital load do not show any child nodes.
+// if open, start icrementing the height by 1
+// plop in the child nodes and detect its height,
+// make sure the container height does not exceed the child nodes height.
+
+
 export interface IOpenProps {
   if? : boolean;
   className? : any;
@@ -15,7 +23,59 @@ export interface IOpenProps {
 
 class Open extends React.Component<IOpenProps, any>{
 
-  public refOpen : any;
+  public refs : any;
+
+  constructor() {
+    super(); 
+    this.state = {
+      height : 0
+    }
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+     const self = this;
+     const props = this.props;
+     let clientHeight = self.refs.Open && self.refs.Open.children.length ? self.refs.Open.children[0].clientHeight : 0;
+     this.setState({
+       height: nextProps.if ? self.recursiveHeight(0, clientHeight) + 'px' : 0
+     })
+  }
+
+  recursiveHeight(lastHeight, initialHeight) {
+
+      let currentHeight = lastHeight || 0;
+
+      if (currentHeight !== initialHeight) {
+            return this.recursiveHeight(currentHeight + 1, initialHeight);
+        } else {
+            return currentHeight;
+        }
+    }
+
+  shouldComponentUpdate(){
+    return true;
+  }
+
+ handleResize(e) {
+    if(this.props.if) {
+      const self = this;
+     const props = this.props;
+     let clientHeight = self.refs.Open && self.refs.Open.children.length ? self.refs.Open.children[0].clientHeight : 0;
+     
+     this.setState({
+       height: self.recursiveHeight(0, clientHeight) + 'px'
+     })
+    } else return null
+  }
+
+  componentDidMount() {
+    this.refs.Open.addEventListener('DOMSubtreeModified', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    this.refs.Open.removeEventListener('DOMSubtreeModified', this.handleResize);
+  }
 
   render(){
      const self = this;
@@ -31,10 +91,17 @@ class Open extends React.Component<IOpenProps, any>{
         {'fill': (props.fill)},
         props.className
      );
+      
+     let clientHeight = self.refs.Open && self.refs.Open.children.length ? self.refs.Open.children[0].clientHeight : 0;
+     let openStyle;
+
+      openStyle = {
+        height : self.state.height
+      }
 
      return(
-       <div ref="Open" className={OpenClass}>
-         {props.if ? props.children : null}
+       <div ref="Open" className={OpenClass} style={openStyle}>
+         <div>{props.children}</div>
        </div>
      )
   }
