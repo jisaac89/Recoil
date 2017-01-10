ebd0dac053475eddbef0ff3b4e88bc17e88cd258
jisaac89 <josephisaac89@gmail.com>
2017-01-10 15:10:24 -0700

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -14,21 +14,20 @@ module.exports = {
         filename: 'index.js',
         publicPath: 'dist'
     },
-    externals: [{
-        'react': {
+    externals: {
+        react: {
             root: 'React',
             commonjs2: 'react',
             commonjs: 'react',
             amd: 'react'
-        }
-    }, {
+        },
         'react-dom': {
             root: 'ReactDOM',
             commonjs2: 'react-dom',
             commonjs: 'react-dom',
             amd: 'react-dom'
         }
-    }],
+    },
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             sourceMap: false,
