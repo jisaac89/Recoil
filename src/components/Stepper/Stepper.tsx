47b02edac1ef42d73ae6657b8d7510314871e2b7
jisaac89 <josephisaac89@gmail.com>
2018-04-04 22:13:34 -0700

    stepper align top added

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -9,55 +9,56 @@ export interface IStepperProps {
     stepIndex: number;
     children?: any;
     vertical?: boolean;
+    align?: 'top' | 'center';
 }
 
 class Stepper extends React.Component<IStepperProps, any>{
 
-  public refOpen : any;
+    public refOpen: any;
 
-  render(){
-     const self = this;
-     const props = self.props;
+    render() {
+        const self = this;
+        const props = self.props;
 
-     let {className} = props;
+        let { className } = props;
 
-     let itemArray : Array<any> = [];
+        let itemArray: Array<any> = [];
 
-     let createList = (item : Array<any>, index: string | number) => {
-         if (index === props.children.length -1) {
-             itemArray.push(
-                 <Shrink key={index} opacity={50} scale={1} if={index > props.stepIndex}>
-                     <div>
-                         {item}
-                     </div>
-                 </Shrink>
-             )
-         } else {
-             itemArray.push(
-                 <Shrink key={index} opacity={50} scale={1} if={index > props.stepIndex}>
-                     <div>
-                         {item} 
-                     </div>
-                 </Shrink>,
-                 <Shrink key={index} opacity={50} scale={1} if={index > props.stepIndex}>
-                     <div className={index > props.stepIndex - 1 ? "flex-step e-gray" : "flex-step"}>
-                     </div>
-                 </Shrink>
-             )
-         }
-     } 
+        let createList = (item: Array<any>, index: string | number) => {
+            if (index === props.children.length - 1) {
+                itemArray.push(
+                    <Shrink key={index} opacity={50} scale={1} if={index > props.stepIndex}>
+                        <div>
+                            {item}
+                        </div>
+                    </Shrink>
+                )
+            } else {
+                itemArray.push(
+                    <Shrink key={index} opacity={50} scale={1} if={index > props.stepIndex}>
+                        <div>
+                            {item}
+                        </div>
+                    </Shrink>,
+                    <Shrink key={index} opacity={50} scale={1} if={index > props.stepIndex}>
+                        <div className={index > props.stepIndex - 1 ? "flex-step e-gray" : "flex-step"}>
+                        </div>
+                    </Shrink>
+                )
+            }
+        }
 
-     props.children.map(createList);
+        props.children.map(createList);
 
-     return(
-         <div className={"r-Stepper dblock w100 " + className}>
-             {props.title ? <h5>{props.title}</h5> : null}
-             <Align vertical={props.vertical}>
-                 {itemArray}
-             </Align>
-         </div>
-     )
-  }
+        return (
+            <div className={"r-Stepper dblock w100 " + className}>
+                {props.title ? <h5>{props.title}</h5> : null}
+                <Align className={props.align === 'top' ? 'e-align-top' : 'middle'} vertical={props.vertical}>
+                    {itemArray}
+                </Align>
+            </div>
+        )
+    }
 }
 
 export default Stepper;
