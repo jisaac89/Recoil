182b9542f3b90e86a8bae4cfcf647948d155c778
Joseph Isaac <jisaac@cubexsystem.com>
2018-07-22 13:53:59 -0700

    added pos relative prop to wizard

--- src/components/Wizard/Wizard.tsx
+++ src/components/Wizard/Wizard.tsx
@@ -4,20 +4,21 @@ import * as classNames from 'classnames';
 import Pager from '../Pager/Pager';
 
 export interface IWizardProps {
-  slideIndex : number;
-  children ?: any;
-  vertical ? : boolean;
-  className ? : string;
-  style ? : Object;
-  mobile? : boolean;
-  animate? : boolean;
-  fill ? : boolean;
+  slideIndex: number;
+  children?: any;
+  vertical?: boolean;
+  className?: string;
+  style?: Object;
+  mobile?: boolean;
+  animate?: boolean;
+  fill?: boolean;
   overflow?: boolean;
   flex?: boolean;
-  paginate? : boolean;
+  paginate?: boolean;
+  selectedIsRelative?: boolean;
 }
 
-const WizardSlide : any = (props : any) => {
+const WizardSlide: any = (props: any) => {
   return (
     <div className={props.className}>
       {props.visible ? props.children : null}
@@ -29,20 +30,20 @@ export default class Wizard extends React.Component<IWizardProps, any>{
   public static defaultProps = {
     slideIndex: 0
   }
-  constructor(props : IWizardProps) {
+  constructor(props: IWizardProps) {
     super(props);
     this.state = {
-      slideIndex : props.slideIndex || 0
+      slideIndex: props.slideIndex || 0
     }
   }
 
-  componentWillReceiveProps(nextProps: IWizardProps){
-    if(nextProps.slideIndex !== this.state.slideIndex){
+  componentWillReceiveProps(nextProps: IWizardProps) {
+    if (nextProps.slideIndex !== this.state.slideIndex) {
       this.gotoSlideIndex(nextProps.slideIndex);
     }
   }
 
-  gotoSlideIndex(slideIndex : number){
+  gotoSlideIndex(slideIndex: number) {
     this.setState({
       slideIndex: slideIndex
     })
@@ -58,51 +59,52 @@ export default class Wizard extends React.Component<IWizardProps, any>{
       'r-Wizard',
       { 'e-fill': (props.fill) },
       { 'e-flex': (props.flex || props.paginate) },
-      {'e-overflow': (props.overflow)}
+      { 'e-overflow': (props.overflow) }
     );
 
-    let createSlidesPartial = (item : Array<any>, index : string | number) => {
+    let createSlidesPartial = (item: Array<any>, index: string | number) => {
 
       let selected = state.slideIndex === index;
 
       let wizardSlideClass = classNames(
         'r-WizardSlide',
-        {'e-selected' : (state.slideIndex === index)},
-        {'e-backward' : (state.slideIndex > index)},
-        {'e-forward' : (state.slideIndex < index)},
-        {'e-vertical' : (props.vertical)},
-        {'e-dont-animate' : (props.animate === false)},
+        { 'e-selected': (state.slideIndex === index) },
+        { 'e-backward': (state.slideIndex > index) },
+        { 'e-forward': (state.slideIndex < index) },
+        { 'e-vertical': (props.vertical) },
+        { 'e-dont-animate': (props.animate === false) },
+        { 'e-selected-relative': (props.selectedIsRelative === true && state.slideIndex === index) },
         props.className
       );
 
       return (
-          <WizardSlide visible={selected} className={wizardSlideClass} key={index}>
-              {item}
-          </WizardSlide>
+        <WizardSlide visible={selected} className={wizardSlideClass} key={index}>
+          {item}
+        </WizardSlide>
       )
-    }; 
+    };
 
     if (props.children.length > 1) {
-      return(
+      return (
         <div ref="r-Wizard" className={wizardClass} style={props.style}>
           <div ref='r-Wizard__track' className="r-Wizard__track">
             {props.children.map(createSlidesPartial)}
           </div>
-           {props.paginate ? 
-             <Pager 
-                numberOfPages={props.children.length}
-                currentPage={state.slideIndex}
-                gotoPage={this.gotoSlideIndex.bind(this)}
-                firstPage={this.gotoSlideIndex.bind(this, 0)}
-                lastPage={this.gotoSlideIndex.bind(this, props.children.length - 1)}
-                nextPage={this.gotoSlideIndex.bind(this, this.state.slideIndex + 1)}
-                previousPage={this.gotoSlideIndex.bind(this, this.state.slideIndex - 1)}
-                hidePageSize
-              /> : null
-            }
+          {props.paginate ?
+            <Pager
+              numberOfPages={props.children.length}
+              currentPage={state.slideIndex}
+              gotoPage={this.gotoSlideIndex.bind(this)}
+              firstPage={this.gotoSlideIndex.bind(this, 0)}
+              lastPage={this.gotoSlideIndex.bind(this, props.children.length - 1)}
+              nextPage={this.gotoSlideIndex.bind(this, this.state.slideIndex + 1)}
+              previousPage={this.gotoSlideIndex.bind(this, this.state.slideIndex - 1)}
+              hidePageSize
+            /> : null
+          }
         </div>
       );
-    } else{
+    } else {
       return props.children;
     }
   }
