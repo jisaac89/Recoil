e39c7df468f30c00029fe1f77918f429723c6ee6
jisaac89 <josephisaac89@gmail.com>
2018-10-11 16:03:11 -0700

    slight update to notifcation comp

---
+++ src/components/Notifications/Notification.tsx
@@ -0,0 +1,96 @@
+import * as React from 'react';
+import Toolbar from '../Toolbar/Toolbar';
+
+export type NotificationType = 'success' | 'primary' | 'error' | 'default';
+
+export interface INotificationItem {
+  template: string | JSX.Element;
+  type?: NotificationType;
+  id?: number;
+}
+
+export interface INotificationProps {
+  item: INotificationItem;
+}
+
+export interface INotificationState {
+  view?: 'visible' | 'hiding' | 'removed';
+  time?: number;
+  isOn?: boolean;
+  start: number;
+}
+
+export default class Notification extends React.Component<INotificationProps, INotificationState> {
+  secondsRemaining: any;
+  timer: any;
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      isOn: false,
+      start: 0,
+      view: 'visible'
+    };
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+  }
+  startTimer() {
+    this.setState({
+      isOn: true,
+      time: this.state.time,
+      start: Date.now() - this.state.time
+    });
+    this.timer = setInterval(
+      () =>
+        this.setState({
+          time: Date.now() - this.state.start
+        }),
+      1
+    );
+  }
+  stopTimer() {
+    this.setState({ isOn: false });
+    clearInterval(this.timer);
+  }
+  resetTimer() {
+    this.setState({ time: 0, isOn: false });
+  }
+
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  stopAndResetTimer() {
+    this.stopTimer();
+    this.resetTimer();
+  }
+
+  render() {
+    const self = this;
+    const props = self.props;
+    let { time, view } = this.state;
+
+    let animationClass;
+
+    if (time < 3000) {
+      animationClass = 'fadeInUp';
+    } else if (time < 4000 && time > 3000) {
+      animationClass = 'fadeOut';
+    } else {
+      animationClass = 'hide';
+    }
+
+    return (view === 'visible' && animationClass !== 'hide') || (view === 'hiding' && animationClass !== 'hide') ? (
+      <Toolbar
+        onMouseEnter={this.stopAndResetTimer.bind(this)}
+        onMouseLeave={this.startTimer.bind(this)}
+        block
+        textCenter
+        className={'p10 m0 w100 e-light e-dropShadow animated' + ' ' + animationClass}
+      >
+        {props.item.template}
+      </Toolbar>
+    ) : null;
+  }
+}
--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 
-import Button from '../Button/Button';
-import Toolbar from '../Toolbar/Toolbar';
+import * as classNames from 'classnames';
+import Portal from '../Portal/Portal';
+import Notification from './Notification';
 
-export type NotificationType = 'success' | 'primary' | 'error' | 'default';
+export interface INotificationsProps {
+  dataSource: INotificationItem[];
+  className: string;
+}
 
 export interface INotificationItem {
   template: string | JSX.Element;
@@ -11,88 +15,27 @@ export interface INotificationItem {
   id?: number;
 }
 
-export interface INotificationProps {
-  item: INotificationItem;
-}
+export type NotificationType = 'success' | 'primary' | 'error' | 'default';
 
-export interface INotificationState {
-  view?: 'visible' | 'hiding' | 'removed';
-  minutes?: string;
-  seconds?: string;
-  value: any;
-}
+export interface INotificationsState {}
 
-export default class Notification extends React.Component<INotificationProps, any> {
-  secondsRemaining: any;
-  timer: any;
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: 0,
-      isOn: false,
-      start: 0,
-      view: 'visible'
-    };
-    this.startTimer = this.startTimer.bind(this);
-    this.stopTimer = this.stopTimer.bind(this);
-    this.resetTimer = this.resetTimer.bind(this);
-  }
-  startTimer() {
-    this.setState({
-      isOn: true,
-      time: this.state.time,
-      start: Date.now() - this.state.time
-    });
-    this.timer = setInterval(
-      () =>
-        this.setState({
-          time: Date.now() - this.state.start
-        }),
-      1
-    );
-  }
-  stopTimer() {
-    this.setState({ isOn: false });
-    clearInterval(this.timer);
-  }
-  resetTimer() {
-    this.setState({ time: 0, isOn: false });
-  }
-
-  componentDidMount() {
-    this.startTimer();
-  }
-
-  stopAndResetTimer() {
-    this.stopTimer();
-    this.resetTimer();
-  }
-
+export default class Notifications extends React.Component<INotificationsProps, INotificationsState> {
   render() {
-    const self = this;
-    const props = self.props;
-    let { time, view } = this.state;
+    let { className, dataSource } = this.props;
 
-    let animationClass;
+    let notificationClass = classNames('r-Notifications', 'w400px', 'dblock', className);
 
-    if (time < 3000) {
-      animationClass = 'fadeInUp';
-    } else if (time < 4000 && time > 3000) {
-      animationClass = 'fadeOut';
-    } else {
-      animationClass = 'hide';
-    }
-
-    return (view === 'visible' && animationClass !== 'hide') || (view === 'hiding' && animationClass !== 'hide') ? (
-      <Toolbar
-        onMouseEnter={this.stopAndResetTimer.bind(this)}
-        onMouseLeave={this.startTimer.bind(this)}
-        block
-        textCenter
-        className={'p10 m0 w100 e-light e-dropShadow animated' + ' ' + animationClass}
-      >
-        {props.item.template}
-      </Toolbar>
-    ) : null;
+    return (
+      <Portal
+        open={true}
+        portalTemplate={
+          <div className={notificationClass}>
+            {dataSource.map((item, index) => {
+              return <Notification item={item} key={index} />;
+            })}
+          </div>
+        }
+      />
+    );
   }
 }
